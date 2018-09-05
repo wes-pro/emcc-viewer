e87092faa67ecd13f8383ab0aa8a82398681dde7
Michal Wesolowski <michal@wespro.pl>
2018-09-05 08:57:08 +0200

    Before POUG 2018

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-db_tns = 'OMR_A2T'
+db_tns = 'OMR'
 db_user = 'poug'
-db_pass = 'welcome1'
+db_pass = 'beer'
 
 target_types = ['host', 'oracle_database', 'oracle_exadata']
 target_typenames_map = {
